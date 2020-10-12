1247e52f103f141d2daa24b95b515784b173856f
hbened01 <humbertobenedito@gmail.com>
2020-10-12 17:00:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('acá empieza todo!!');
+// alert('que es esta vaina de transpilar')
+console.log('hola de nuevo');
+console.warn('error en la consola salir corriendo!!');
+console.error('sal corriendo de aqui');
+console.clear();
---
+++ index.ts
@@ -0,0 +1,9 @@
+console.log('acá empieza todo!!')
+// alert('que es esta vaina de transpilar')
+
+console.log('hola de nuevo')
+console.warn('error en la consola salir corriendo!!')
+
+console.error('sal corriendo de aqui')
+console.clear();
+
